a0aa8750dd51ce3449a2b9507194695425364280
lucas <lucas.belini@escola.pr.gov.br>
2024-04-05 09:02:50 -0300

    joia

--- prototipo/MainPage.xaml.cs
+++ prototipo/MainPage.xaml.cs
@@ -11,12 +11,22 @@ namespace prototipo
             InitializeComponent();
 
             shaco = new Shaco();
+            
+            
+            shaco.SetFome(shaco.GetFome()+0.1);
+            progressBar.Progress = shaco.GetFome();
 
             
             var timer = Application.Current.Dispatcher.CreateTimer();
-            timer.Interval = TimeSpan.FromSeconds(1); 
+            timer.Interval = TimeSpan.FromSeconds(10); 
             timer.Tick += (s, e) => PassouTempo(); 
             timer.Start(); 
+        }
+
+          void botao1(object sender, EventArgs args)
+        {
+            shaco.SetFome(shaco.GetFome() + 0.1);
+            progressBar.Progress = shaco.GetFome();
         }
 
         
